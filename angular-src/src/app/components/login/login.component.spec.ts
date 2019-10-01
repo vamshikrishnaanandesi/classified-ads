416958e8a6a8b7fd556f894281083698584e6422
Reshma Shaik <reshmashaik462@gmail.com>
2019-10-01 13:02:33 +0530

    login and register test cases done

--- angular-src/src/app/components/login/login.component.spec.ts
+++ angular-src/src/app/components/login/login.component.spec.ts
@@ -1,8 +1,10 @@
+
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ToastrModule } from 'ngx-toastr';
-import {BrowserModule, By } from '@angular/platform-browser';
-import {ReactiveFormsModule, FormsModule,FormBuilder, FormGroup, Validators,FormControl} from '@angular/forms';
-import {DebugElement} from '@angular/core';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { DebugElement } from '@angular/core';
 import { AppRoutingModule } from '../../app-routing.module';
 import { RegisterComponent } from '../register/register.component';
 import { LoginComponent } from '../login/login.component';
@@ -12,34 +14,34 @@ import { ListviewComponent } from '../listview/listview.component';
 import { PostviewComponent } from '../postview/postview.component';
 import { SearchbarComponent } from '../searchbar/searchbar.component';
 import { HttpClientModule } from '@angular/common/http';
-import { OwlCarousel } from 'ngx-owl-carousel';
+import { OwlModule } from 'ngx-owl-carousel';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   let de: DebugElement;
-  let el:HTMLElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent,LoginComponent,
+      declarations: [RegisterComponent, LoginComponent,
         NavbarComponent,
-    GridviewComponent,
-    ListviewComponent,
-    PostviewComponent,
-    SearchbarComponent,
+        GridviewComponent,
+        ListviewComponent,
+        PostviewComponent,
+        SearchbarComponent,
       ],
       providers: [
         FormBuilder
       ],
-      imports:[FormsModule,HttpClientModule,AppRoutingModule,BrowserModule,ReactiveFormsModule,ToastrModule.forRoot()]
+      imports: [OwlModule, FormsModule, HttpClientModule, AppRoutingModule, BrowserModule, ReactiveFormsModule, ToastrModule.forRoot()]
     })
-    .compileComponents().then(()=>{
-      fixture = TestBed.createComponent(LoginComponent);
-      component = fixture.componentInstance;
-      component.ngOnInit();
-      de= fixture.debugElement.query(By.css('form'));
-      el = de.nativeElement;
-    })
+      .compileComponents().then(() => {
+        fixture = TestBed.createComponent(LoginComponent);
+        component = fixture.componentInstance;
+        component.ngOnInit();
+        de = fixture.debugElement.query(By.css('form'));
+        el = de.nativeElement;
+      })
   }));
 
 
@@ -47,26 +49,26 @@ describe('LoginComponent', () => {
   //   expect(component.text).toEqual('Login')
   // }));
 
-  it(`should set submitted to true`,async(()=>{
+  it(`should set submitted to true`, async(() => {
     component.loginAuth();
     expect(component.submitted).toBeTruthy()
   }));
 
-  it(`should call the loginAuth method`,async(()=>{
-      fixture.detectChanges();
-      spyOn(component,'loginAuth');
-      el = fixture.debugElement.query(By.css('button')).nativeElement;
-      el.click();
-      expect(component.loginAuth).toHaveBeenCalledTimes(1)
+  it(`should call the loginAuth method`, async(() => {
+    fixture.detectChanges();
+    spyOn(component, 'loginAuth');
+    el = fixture.debugElement.query(By.css('button')).nativeElement;
+    el.click();
+    expect(component.loginAuth).toHaveBeenCalledTimes(1)
   }))
 
-  it(`form should be invalid`,async(()=>{
+  it(`form should be invalid`, async(() => {
     component.loginForm.controls['email'].setValue('')
     component.loginForm.controls['password'].setValue('')
     expect(component.loginForm.valid).toBeFalsy()
   }));
 
-  it(`form should be valid`,async(()=>{
+  it(`form should be valid`, async(() => {
     component.loginForm.controls['email'].setValue('hemantham02@gmail.com')
     component.loginForm.controls['password'].setValue('hemanth@002')
     expect(component.loginForm.valid).toBeTruthy()
--- angular-src/src/app/components/register/register.component.spec.ts
+++ angular-src/src/app/components/register/register.component.spec.ts
@@ -1,8 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ToastrModule } from 'ngx-toastr';
-import {BrowserModule, By } from '@angular/platform-browser';
-import {ReactiveFormsModule, FormsModule,FormBuilder, FormGroup, Validators,FormControl} from '@angular/forms';
-import {DebugElement} from '@angular/core';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { DebugElement } from '@angular/core';
 import { AppRoutingModule } from '../../app-routing.module';
 import { RegisterComponent } from './register.component';
 import { LoginComponent } from '../login/login.component';
@@ -12,34 +12,34 @@ import { ListviewComponent } from '../listview/listview.component';
 import { PostviewComponent } from '../postview/postview.component';
 import { SearchbarComponent } from '../searchbar/searchbar.component';
 import { HttpClientModule } from '@angular/common/http';
-import { OwlCarousel } from 'ngx-owl-carousel';
+import { OwlModule } from 'ngx-owl-carousel';
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
   let de: DebugElement;
-  let el:HTMLElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent,LoginComponent,
+      declarations: [RegisterComponent, LoginComponent,
         NavbarComponent,
-    GridviewComponent,
-    ListviewComponent,
-    PostviewComponent,
-    SearchbarComponent,
+        GridviewComponent,
+        ListviewComponent,
+        PostviewComponent,
+        SearchbarComponent,
       ],
       providers: [
         FormBuilder
       ],
-      imports:[FormsModule,HttpClientModule,AppRoutingModule,BrowserModule,ReactiveFormsModule,ToastrModule.forRoot()]
+      imports: [OwlModule, FormsModule, HttpClientModule, AppRoutingModule, BrowserModule, ReactiveFormsModule, ToastrModule.forRoot()]
     })
-    .compileComponents().then(()=>{
-      fixture = TestBed.createComponent(RegisterComponent);
-      component = fixture.componentInstance;
-      component.ngOnInit();
-      de= fixture.debugElement.query(By.css('form'));
-      el = de.nativeElement;
-    })
+      .compileComponents().then(() => {
+        fixture = TestBed.createComponent(RegisterComponent);
+        component = fixture.componentInstance;
+        component.ngOnInit();
+        de = fixture.debugElement.query(By.css('form'));
+        el = de.nativeElement;
+      })
   }));
 
 
@@ -47,20 +47,20 @@ describe('RegisterComponent', () => {
   //   expect(component.text).toEqual('Register')
   // }));
 
-  it(`should set submitted to true`,async(()=>{
+  it(`should set submitted to true`, async(() => {
     component.register();
     expect(component.submitted).toBeTruthy()
   }));
 
-  it(`should call the register method`,async(()=>{
-      fixture.detectChanges();
-      spyOn(component,'register');
-      el = fixture.debugElement.query(By.css('button')).nativeElement;
-      el.click();
-      expect(component.register).toHaveBeenCalledTimes(0)
+  it(`should call the register method`, async(() => {
+    fixture.detectChanges();
+    spyOn(component, 'register');
+    el = fixture.debugElement.query(By.css('button')).nativeElement;
+    el.click();
+    expect(component.register).toHaveBeenCalledTimes(1)
   }))
 
-  it(`form should be invalid`,async(()=>{
+  it(`form should be invalid`, async(() => {
     component.registerForm.controls['username'].setValue('')
     component.registerForm.controls['email'].setValue('')
     component.registerForm.controls['age'].setValue('')
@@ -70,7 +70,7 @@ describe('RegisterComponent', () => {
     expect(component.registerForm.valid).toBeFalsy()
   }));
 
-  it(`form should be valid`,async(()=>{
+  it(`form should be valid`, async(() => {
     component.registerForm.controls['username'].setValue('hemanth')
     component.registerForm.controls['email'].setValue('hemantham02@gmail.com')
     component.registerForm.controls['age'].setValue(23)
