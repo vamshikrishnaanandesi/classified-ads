c8cef4e6e7080c871fbb3e39016b508809aab43a
Reshma Shaik <reshmashaik462@gmail.com>
2019-10-02 00:30:03 +0530

    card css changes

--- angular-src/src/app/components/postview/postview.component.ts
+++ angular-src/src/app/components/postview/postview.component.ts
@@ -15,8 +15,8 @@ export class PostviewComponent implements OnInit {
   imageObject: any = [];
   myCarouselOptions: { items: number; dots: boolean; nav: boolean; };
   mySlideOptions: { items: number; dots: boolean; nav: boolean; };
-  mySlideImages: string[];
-  myCarouselImages: string[];
+  mySlideImages: any = [];
+  myCarouselImages: any = [];
   constructor(private _location: Location, private router: Router, private activedRoute: ActivatedRoute, private commonService: CommonService) { }
 
   ngOnInit() {
@@ -24,15 +24,15 @@ export class PostviewComponent implements OnInit {
     this.activedRoute.params.subscribe(data => {
       this.commonService.getOnead(data.id).subscribe(data => {
         this.ad = data.data;
-        this.ad.images.forEach(element => {
-          this.mySlideImages.push(element.url);
-          this.myCarouselImages.push(element.url);
+        console.log(this.ad.images)
+        this.ad.images.forEach((element: any) => {
+          // this.mySlideImages.push('https://picsum.photos/id/1/640/480');
+          // this.myCarouselImages.push('https://picsum.photos/id/1/640/480');
         });
       })
-    })
-
-    // this.mySlideImages = ['https://picsum.photos/id/1/640/480', 'https://picsum.photos/id/2/640/480', 'https://picsum.photos/id/3/640/480'];
-    // this.myCarouselImages = ['https://picsum.photos/id/1/640/480', 'https://picsum.photos/id/2/640/480', 'https://picsum.photos/id/3/640/480'];
+      this.mySlideImages.push('https://picsum.photos/id/1/640/480','https://picsum.photos/id/1/640/480');
+      this.myCarouselImages.push('https://picsum.photos/id/1/640/480','https://picsum.photos/id/1/640/480')
+    });
     this.mySlideOptions = { items: 1, dots: true, nav: false };
   }
 
